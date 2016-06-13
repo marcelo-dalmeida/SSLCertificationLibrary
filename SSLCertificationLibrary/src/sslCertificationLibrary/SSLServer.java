53a6d524ae74c32e0aed4c928af1d081e7b49072
md@id.uff.br <md@id.uff.br>
2016-06-13 11:52:44 -0400

    Update SSLServer.java

--- SSLCertificationLibrary/src/sslCertificationLibrary/SSLServer.java
+++ SSLCertificationLibrary/src/sslCertificationLibrary/SSLServer.java
@@ -33,56 +33,56 @@ public class SSLServer
 
 	public static void main(String[] args) throws Exception
 	{
-
+		
 		int intSSLport = 4443; // Port where the SSL Server needs to listen for new requests from the client
-
-	
+		
+		
 		// Registering the JSSE provider
 		Security.addProvider(new Provider());
-
+		
 		//Specifying the Keystore details
 		System.setProperty("javax.net.ssl.keyStore","keystore.jks");
 		System.setProperty("javax.net.ssl.keyStorePassword","keystore");
-
+		
 		// Enable debugging to view the handshake and communication which happens between the SSLClient and the SSLServer
 		System.setProperty("javax.net.debug","all");
-	
-	
+		
+		
 		try 
 		{
 			// Initialize the Server Socket
 			SSLServerSocketFactory sslServerSocketfactory = (SSLServerSocketFactory)SSLServerSocketFactory.getDefault();
 			SSLServerSocket sslServerSocket = (SSLServerSocket)sslServerSocketfactory.createServerSocket(intSSLport);
 			SSLSocket sslSocket = (SSLSocket)sslServerSocket.accept();
-
+			
 			// Create Input / Output Streams for communication with the client
 			while(true)
 			{
 				PrintWriter out = new PrintWriter(sslSocket.getOutputStream(), true);
-		        BufferedReader in = new BufferedReader(
-		        		new InputStreamReader(
-		        				sslSocket.getInputStream()));
-		        String inputLine;
-		        //String outputLine;
-	
-		        while ((inputLine = in.readLine()) != null) 
-		        {
-		             out.println(inputLine);
-		             System.out.println(inputLine);
-		        }
-
-		        // Close the streams and the socket
-		        out.close();
-		        in.close();
-		        sslSocket.close();
-		        sslServerSocket.close();
+				BufferedReader in = new BufferedReader(
+					new InputStreamReader(
+						sslSocket.getInputStream()));
+				String inputLine;
+				//String outputLine;
+				
+				while ((inputLine = in.readLine()) != null) 
+				{
+					out.println(inputLine);
+					System.out.println(inputLine);
+				}
+				
+				// Close the streams and the socket
+				out.close();
+				in.close();
+				sslSocket.close();
+				sslServerSocket.close();
 			}
 		}
 		catch(Exception exp)
 		{
 			PrivilegedActionException priexp = new PrivilegedActionException(exp);
 			System.out.println(" Priv exp --- " + priexp.getMessage());
-
+			
 			System.out.println(" Exception occurred .... " +exp);
 			exp.printStackTrace();
 		}
